a59234430ec4865f8b7b17ebaccfdf001b8d2285
NaveshSapad <navesh.sapad@gmail.com>
2021-02-03 15:23:34 +0530

    ''

--- test.py
+++ test.py
@@ -10,32 +10,54 @@ import shelve
 import json
 from datetime import datetime as dt
 import time
+import altair as alt
+from PIL import Image
 
 
-st.markdown('<body bgcolor="black" ></body>',unsafe_allow_html=True)
 
 
-st1=st.empty()
-st_input=st.empty()
+st_hive_username=st.sidebar.empty()
+st_select_token=st.sidebar.empty()
+st_select_symbol=st.sidebar.empty()
+
+st_image=st.sidebar.empty()
+
 df=pd.read_csv('test.csv')
 df=df.drop('Unnamed: 0',axis=1)
-name=st_input.text_input('Enter your Hive username')
 
-st.write('Your selected username='+name)
-df1=df[df['to']==name]
+sym_list=list(set(df['symbol']))
 
-df1['quantity']=pd.to_numeric(df1['quantity'])
 
-sym=st.text_input('Enter Symbol')
+hive_user=st_hive_username.text_input('Enter your Hive username','taskmaster4450')
+token=st_select_token.selectbox('Select the token you wish to see dividends for',['BRO','INDEX'])
+sym= st_select_symbol.selectbox('Select SYMBOL',sym_list)
 
-df_sym=df1[df1['symbol']==sym]
-df_sym=df_sym.set_index(df_sym['date'])
+
+if st_select_token:
+    if(token=='BRO'):
+        st_image.image('bro.png',use_column_width=True)
+    else:
+        st_image.image('tweet.png',use_column_width=True)
+
+df_user_details=df[df['to']==hive_user]
+df_user_details['quantity']=pd.to_numeric(df_user_details['quantity'])
+
+df_sym=df_user_details[df_user_details['symbol']==sym]
 sum_sym=df_sym['quantity'].sum()
+
+if st.checkbox('Show table: Last 10 days '+sym+' payout'):
+    st.table(df_sym.tail(10))
+
+st.markdown('<hr>',unsafe_allow_html=True)
+
+
 if sym!='':
-    st.write('Total '+sym+' from Jan 1 to Feb 1 : '+str(sum_sym)+' '+sym)
+    st.write('<div class="comments"><center>Total '+sym+' from Jan 1 to Feb 1 : '+str(sum_sym)+' '+sym+'</center></div>',unsafe_allow_html=True)
 else:
     st.write('Please enter Symbol')
-st.line_chart(df_sym['quantity'])
 
+    
+c = alt.Chart(df_sym).mark_line(point=True).encode(x='date', y='quantity',color='symbol',tooltip=['quantity']).interactive()
 
+st.write(c)
 
